ab38f3a272d8d408c610f9d4dd665c54429ad3b2
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2023-10-18 14:47:45 +0800

    Consider HackMD's team note URL

--- _scripts/create_new_post.sh
+++ _scripts/create_new_post.sh
@@ -14,6 +14,6 @@ set -a; . ./.env; set +a
 HACKMD_API_TOKEN=${HACKMD_API_TOKEN}
 HACKMD_POST_URL=${1}
 POST_FILENAME=${2}
-HACKMD_NOTE_ID=$(echo ${HACKMD_POST_URL} | sed -r 's,https://hackmd.io/([A-Za-z0-9_-]+)[\?#]?.*,\1,g')
+HACKMD_NOTE_ID=$(echo ${HACKMD_POST_URL} | sed -r 's,https://hackmd.io/(@.+/)?([A-Za-z0-9_-]+)[\?#]?.*,\2,g')
 
 curl --fail --silent --show-error --location --header "Authorization: Bearer ${HACKMD_API_TOKEN}" "https://api.hackmd.io/v1/notes/${HACKMD_NOTE_ID}" | jq --raw-output '.content' > ./source/_posts/${POST_FILENAME}.md
