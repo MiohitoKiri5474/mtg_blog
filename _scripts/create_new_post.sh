8efa0897c03af28da002500dd43ca9d3408d5fb9
Perry Chou <906135+mbcoguno@users.noreply.github.com>
2023-10-20 17:44:50 +0800

    Merge all sed commands into one process

--- _scripts/create_new_post.sh
+++ _scripts/create_new_post.sh
@@ -16,6 +16,7 @@ HACKMD_POST_URL=${1}
 POST_FILENAME=${2}
 HACKMD_NOTE_ID=$(echo ${HACKMD_POST_URL} | sed -r 's,https://hackmd.io/(@.+/)?([A-Za-z0-9_-]+)[\?#]?.*,\2,g')
 
-# This script will remove: contents before front-matter, single line HTML comments,
-# trailing newline. This script also quotes all strings in front-matter.
-curl --fail --silent --show-error --location --header "Authorization: Bearer ${HACKMD_API_TOKEN}" "https://api.hackmd.io/v1/notes/${HACKMD_NOTE_ID}" | jq --raw-output '.content' | sed -n '/^---$/,$p' | sed -e :a -re 's/<!--.*-->//g;/<!--/N;//ba' | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' | yq --front-matter process '.date = (now | tz("Asia/Taipei") | format_datetime("2006-01-02 15:04:05")) | .. style="double"' | tee ./source/_posts/${POST_FILENAME}.md > /dev/null
+# This script will remove: contents before front-matter, HTML comments if possible,
+# and trailing newline. This script also quotes all strings in front-matter.
+# Mostly sed scripts are reference from <https://www.pement.org/sed/sed1line.txt>
+curl --fail --silent --show-error --location --header "Authorization: Bearer ${HACKMD_API_TOKEN}" "https://api.hackmd.io/v1/notes/${HACKMD_NOTE_ID}" | jq --raw-output '.content' | sed -e '/^---$/,$!d' -e :a -e 's/<!--.*-->//g;/<!--/N;//ba' -e :b -e '/^\n*$/{$d;N;bb' -e '}' | yq --front-matter process '.date = (now | tz("Asia/Taipei") | format_datetime("2006-01-02 15:04:05")) | .. style="double"' | tee ./source/_posts/${POST_FILENAME}.md > /dev/null
